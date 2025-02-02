d5d103a043513175ee671969caf0c55200b31e4c
FerrousInk <moaugustin@proton.me>
2025-02-02 16:28:34 +0100

    Added Boot Animation

---
+++ BUILD.sh
@@ -0,0 +1,11 @@
+for dir in src/*; do
+  if [ -d "$dir" ]; then
+    cd "$dir"
+    makepkg
+    cp -f *.pkg.tar.zst ../../pkg/
+    rm -rf *.pkg.tar.zst pkg src
+    cd ../..
+  fi
+done
+
+repo-add lunitely.db.tar.gz pkg/*

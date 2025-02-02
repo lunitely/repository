8dbd5056cbb0110578dd8e0dd5eb8a6798ec1596
FerrousInk <moaugustin@proton.me>
2025-02-02 19:48:22 +0100

    Updated Cool Stuff

--- BUILD.sh
+++ BUILD.sh
@@ -2,10 +2,10 @@ for dir in src/*; do
   if [ -d "$dir" ]; then
     cd "$dir"
     makepkg
-    cp -f *.pkg.tar.zst ../../pkg/
+    cp -f *.pkg.tar.zst ../../
     rm -rf *.pkg.tar.zst pkg src
     cd ../..
   fi
 done
 
-repo-add lunitely.db.tar.gz pkg/*
+repo-add lunitely.db.tar.gz *.pkg.tar.zst

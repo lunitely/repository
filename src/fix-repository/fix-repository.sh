a6646d668d5f5f9ec0a86d2c002e74754c2f0ba4
Alois~ <moaugustin@proton.me>
2025-02-09 02:41:51 +0100

    Added fix-repository package

---
+++ src/fix-repository/fix-repository.sh
@@ -0,0 +1,23 @@
+KEY=4B758E0A1A2F4146B53E97F6980CD593E882D694
+NAME=lunitely
+
+gpg --recv-keys $KEY
+gpg --output /tmp/$NAME.gpg --armor --export $KEY
+gpg --import /tmp/$NAME.gpg
+
+cp /tmp/$NAME.gpg /usr/share/pacman/keyrings/
+
+pacman-key --recv-keys $KEY
+pacman-key --add /tmp/$NAME.gpg
+pacman-key --finger $KEY
+pacman-key --lsign-key $KEY
+pacman-key --init
+pacman-key --populate $NAME
+
+rm /tmp/$NAME.gpg
+
+echo "" >> /etc/pacman.conf
+echo "[$NAME]" >> /etc/pacman.conf
+echo "Server = https://repo.hackrland.dev/\$arch" >> /etc/pacman.conf
+
+pacman -Syy
